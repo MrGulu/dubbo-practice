f9d246d93442928e44db88a74984e495d5adbeb9
tangwenlong <twlmail@163.com>
2019-05-29 11:39:58 +0800

    BusinessException-new！

--- dubbo-core/src/main/java/cn/tang/exception/BusinessException.java
+++ dubbo-core/src/main/java/cn/tang/exception/BusinessException.java
@@ -8,50 +8,73 @@ import java.util.Map;
 import java.util.Set;
 
 public class BusinessException extends RuntimeException {
-    private static final long serialVersionUID = -5433056591268198786L;
-    private Map<String, String> errorMessages = new HashMap();
+  private static final long serialVersionUID = -5433056591268198786L;
+  private Map<String, String> errorMessages = new HashMap();
 
-    public BusinessException() {
-    }
+  private String errorCode = null;
 
-    public BusinessException(String message) {
-        super(message);
-    }
+  public BusinessException() {
+  }
 
-    public BusinessException(Throwable cause) {
-        super(cause);
-    }
+  public String getErrorCode() {
+    return errorCode;
+  }
 
-    public BusinessException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public BusinessException(String message) {
+    super(message);
 
-    public Map<String, String> getErrorMessages() {
-        return this.errorMessages;
-    }
+  }
 
-    public static long getSerialversionuid() {
-        return -5433056591268198786L;
-    }
+  public BusinessException(IBussinessError bussinessError) {
+    super(bussinessError.getMessage());
+    this.errorCode = bussinessError.getCode();
+  }
 
-    public void setErrorMessages(Set<? extends ConstraintViolation<? extends Object>> violations) {
-        for (ConstraintViolation violation : violations) {
-            String propertyPath = violation.getPropertyPath().toString();
-            String message = violation.getMessage();
-            this.errorMessages.put(propertyPath, message);
-        }
-    }
+  public BusinessException(IBussinessError bussinessError, String... details) {
 
-    public void setErrorMessages(Map<String, String> errorMessages) {
-        this.errorMessages = errorMessages;
-    }
+    super(bussinessError.getMessage(details));
+    this.errorCode = bussinessError.getCode();
+  }
 
-    public void addErrorMessage(String fieldName, String message) {
-        this.errorMessages.put(fieldName, message);
-    }
+  public BusinessException(String errorCode, String message) {
+    super(message);
+    this.errorCode = errorCode;
+  }
 
-    public void addBindingResultTo(BindingResult result) {
-        for (String key : this.errorMessages.keySet())
-            result.rejectValue(key, "", (String) this.errorMessages.get(key));
+  public BusinessException(Throwable cause) {
+    super(cause);
+  }
+
+  public BusinessException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public Map<String, String> getErrorMessages() {
+    return this.errorMessages;
+  }
+
+  public static long getSerialversionuid() {
+    return -5433056591268198786L;
+  }
+
+  public void setErrorMessages(Set<? extends ConstraintViolation<? extends Object>> violations) {
+    for (ConstraintViolation violation : violations) {
+      String propertyPath = violation.getPropertyPath().toString();
+      String message = violation.getMessage();
+      this.errorMessages.put(propertyPath, message);
     }
+  }
+
+  public void setErrorMessages(Map<String, String> errorMessages) {
+    this.errorMessages = errorMessages;
+  }
+
+  public void addErrorMessage(String fieldName, String message) {
+    this.errorMessages.put(fieldName, message);
+  }
+
+  public void addBindingResultTo(BindingResult result) {
+    for (String key : this.errorMessages.keySet())
+      result.rejectValue(key, "", (String) this.errorMessages.get(key));
+  }
 }
